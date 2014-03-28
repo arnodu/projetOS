9e6180474cb30bfd0bef520dd3671b169540ecc5
arnodu <arnodu@github.com>
2014-03-29 00:56:09 +0100

    plus propre

--- trunk/src/thread/self.c
+++
@@ -1,9 +0,0 @@
-#include "thread.h"
-#include "thread_t.h"
-
-#include <assert.h>
-
-thread_t thread_self(void)
-{
-	return ordo_getRunningThread();
-}
--- trunk/src/thread/yield.c
+++
@@ -1,8 +0,0 @@
-#include "thread.h"
-#include "thread_t.h"
-
-int thread_yield(void)
-{
-		ordo_addThread(thread_self());
-	ordo_execute();
-}
