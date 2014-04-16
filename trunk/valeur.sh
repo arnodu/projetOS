e631d9b703057ec54df99b10cf8baf02c440f772
maelorn <maelorn@localhost.localdomain>
2014-04-16 19:02:11 +0200

    ajout d'un script qui trace les courbes correspondant au valeurs

--- trunk/valeur.sh
+++ trunk/valeur.sh
@@ -1,11 +1,14 @@
 
+if [ ! -d data ]; then  
+	mkdir data
+fi
 
 # temps d'execution à nombre de yield constants et à nombre de threads variable
 
 for i in `seq 10 1000`
 do
-	v=$(bin/./31-switch-many 10 $i | cut -d " " -f 6)
-	echo "$i $v" >> time_yield_$1
+	v=$(bin/./31-switch-many $i 10 | cut -d " " -f 6)
+	echo "$i $v" >> data/time_thread_$1
 done
 
 # temps d'execution à nombre de threads constants et à nombre de yield variable
@@ -13,8 +16,8 @@ done
 
 for i in `seq 10 1000`
 do
-	v=$(bin/./31-switch-many $i 10 | cut -d " " -f 6)
-	echo "$i $v" >> time_thread_$1
+	v=$(bin/./31-switch-many 10 $i | cut -d " " -f 6)
+	echo "$i $v" >> data/time_yield_$1
 done
 
 # temps d'execution de fibo
@@ -25,5 +28,6 @@ do
 	bin/./51-fibonacci  $i
 	b=$(date +%s.%N)		
 	c=$(echo "$b - $a" | bc)
-	echo "$i $c" >> time_fibo_$1
+	echo "$i $c" >> data/time_fibo_$1
 done
+
