a2103648186d5a3bfa056a1a4e343bc703c9c197
maelorn <maelorn@localhost.localdomain>
2014-04-18 14:18:32 +0200

    valeur des pthread calculées en meme temps que les threads user

--- trunk/valeur.sh
+++ trunk/valeur.sh
@@ -1,39 +1,47 @@
 
-if [ $# -eq 0 ]
- then
-	echo "veuillez rentrez un parametres: ours pour nos threads pthread pour les threads posix"
 
-else
-	if [ ! -d data ]; then  
-		mkdir data
-	fi
+
+
+if [ ! -d data ]; then  
+	mkdir data
+fi
 
 # temps d'execution à nombre de yield constants et à nombre de threads variable
 
-	for i in `seq 10 1000`
-	do
-		v=$(bin/./31-switch-many $i 10 | cut -d " " -f 6)
-		echo "$i $v" >> data/time_thread_$1
-	done
+for i in `seq 10 1000`
+do
+	
+	v=$(bin/./31-switch-many $i 10 | cut -d " " -f 6)
+	v=$(bin/./31-switch-many-pthread $i 10 | cut -d " " -f 6)
+	echo "$i $v" >> data/time_thread
+	echo "$i $v" >> data/time_thread_pthread
+	
+done
 
 # temps d'execution à nombre de threads constants et à nombre de yield variable
 
 
-	for i in `seq 10 1000`
-	do
-		v=$(bin/./31-switch-many 10 $i | cut -d " " -f 6)
-		echo "$i $v" >> data/time_yield_$1
-	done
+for i in `seq 10 1000`
+do
+	v=$(bin/./31-switch-many 10 $i | cut -d " " -f 6)
+	v=$(bin/./31-switch-many-pthread 10 $i | cut -d " " -f 6)
+	echo "$i $v" >> data/time_yield
+	echo "$i $v" >> data/time_yield_pthread
+done
 
 # temps d'execution de fibo
 
-	for i in `seq 1 16`
-	do
-		a=$(date +%s.%N)	
-		bin/./51-fibonacci  $i
-		b=$(date +%s.%N)		
-		c=$(echo "$b - $a" | bc)
-		echo "$i $c" >> data/time_fibo_$1
-	done
+for i in `seq 1 16`
+do
+	a=$(date +%s.%N)	
+	bin/./51-fibonacci  $i
+	b=$(date +%s.%N)		
+	c=$(echo "$b - $a" | bc)
+	a=$(date +%s.%N)	
+	bin/./51-fibonacci-pthread  $i
+	b=$(date +%s.%N)		
+	d=$(echo "$b - $a" | bc)
+	echo "$i $c" >> data/time_fibo
+	echo "$i $d" >> data/time_fibo_pthread
+done
 
-fi
